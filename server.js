c2bbf495a3cc162d1b4de9fe638e6e2562f2e21a
me222rs <me222rs@student.lnu.se>
2017-03-06 14:50:59 +0100

    gdfgdfgdgd
    gdfg

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 require("./libs/helper").initialize();
-//dfgdfgfghfgtghjbsdfsdfdfsfsdas
+var bodyParser = require("body-parser");
+
+//dfgdfgfghfgtghjbsdfsdfdfsfsdasvxvxvc
+var express = require("express"),
+    app = express(),
+    port = 8080;
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  var server = app.listen(port, function() {
+    console.log('Listening on port %d',port);
+  });
